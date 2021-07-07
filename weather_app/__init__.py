378ee496410e1d407e4336ca87b259e9f152d733
Tim-Abwao <abwaomusungu@gmail.com>
2021-07-07 17:36:25 +0300

    Add weather icons & metrics
    Display icons of the expected weather for the next 1, 6 and 12 hours.
    Add humidity, cloud-cover, air-pressure and wind information.
    Refactor functions and apply black formating.
    Edit docstrings.

--- weather_app/__init__.py
+++ weather_app/__init__.py
@@ -1,17 +1,19 @@
 import os
 from flask import Flask, request, render_template
-from weather_app.utils import greet, get_external_IP_address
+from weather_app.utils import process_weather_forecast, get_external_IP_address
 
 app = Flask(__name__)
-DEPLOY = os.environ.get('DEPLOY')
+DEPLOY = os.environ.get("DEPLOY")
 
 
-@app.route('/')
+@app.route("/")
 def main():
-    if DEPLOY == 'heroku':  # env variable set when deploying to heroku
-        ip_address = request.headers['X-Forwarded-For']
-    else:  # `DEPLOY == local` when running locally
+    if DEPLOY == "heroku":  # env variable set when deploying to heroku
+        ip_address = request.headers["X-Forwarded-For"]
+    else:  # `DEPLOY` is unset when running locally
         # Get an IP other than 'localhost' or '127.0.0.1'
         ip_address = get_external_IP_address()
 
-    return render_template('index.html', weather_info=greet(ip_address))
+    return render_template(
+        "index.html", weather_info=process_weather_forecast(ip_address)
+    )
--- weather_app/utils.py
+++ weather_app/utils.py
@@ -1,156 +1,267 @@
 from io import StringIO
+
 import pandas as pd
 import plotly.express as px
 import requests
 import requests_cache
 
-
+# requests_cache configuration
 FLUSH_PERIOD = 10 * 60  # 10 minutes in seconds
 requests_cache.install_cache(expire_after=FLUSH_PERIOD)
 
 
-def greet(ip_address):
+def process_weather_forecast(ip_address):
     """Create a message with weather and location-related information.
 
     Parameters
     ----------
-    ip_address: str
+    ip_address : str
         A single IPv4/IPv6 address, or a domain name.
 
     Returns
     -------
-    A dictionary of the weather and location information.
+    dict
+        A dictionary of weather and location information.
     """
     location = get_location(ip_address)
-    temperature_data = get_temperature(location['lat'], location['lon'],
-                                       location['timezone'])
-    temp_C = temperature_data.iloc[0]  # temperature in deg C
+    weather_info = get_weather_info(
+        location["lat"], location["lon"], location["timezone"]
+    )
+    temp_C = float(
+        weather_info["current_weather"]["Air temperature"].rstrip("°C")
+    )
     temp_F = convert_to_fahr(temp_C)
 
-    weather_info = {
-        'graphs': plot_forecast(temperature_data),
-        'headline': f"""It's {temp_C :.0f} &deg;C ({temp_F :.0f} &deg;F) in
-                    {location['city']}, {location['country']}
-                    right now.""",
-        'ip_address': ip_address
-    }
-    return weather_info
+    return dict(
+        graphs=plot_forecast(weather_info["temp_time_series"]),
+        headline=(
+            f"It's {temp_C :.0f}°C ({temp_F :.0f}°F) in {location['city']},"
+            f" {location['country']} right now."
+        ),
+        ip_address=ip_address,
+        data=weather_info,
+    )
 
 
 def get_location(ip_address):
     """Get city, country, latitude, longitude and timezone information for a
-     location given an IP address.
+    location given an IP address.
 
     Parameters
     ----------
-    ip_address: str
+    ip_address : str
         A single IPv4/IPv6 address, or a domain name.
 
     Returns
     -------
-    A dictionary of location details.
+    dict
+        A dictionary of location details.
     """
-    response = requests.get(f'http://ip-api.com/json/{ip_address}',
-                            headers={'User-Agent': 'wqu_weather_app'})
-    loc_info = response.json()
-    keys = ('city', 'country', 'lat', 'lon', 'timezone')
-    return {key: loc_info[key] for key in keys}
+    location_info = requests.get(
+        f"http://ip-api.com/json/{ip_address}",
+        headers={"User-Agent": "wqu_weather_app"},
+    ).json()
+
+    return {
+        key: location_info[key]
+        for key in ("city", "country", "lat", "lon", "timezone")
+    }
 
 
-def get_temperature(lat, lon, timezone):
-    """Get temperature time series data for a given location.
+def get_weather_info(lat, lon, timezone):
+    """Get weather forecast data for a given location.
 
     Parameters
     ----------
-    lat, lon: int or float
+    lat, lon : int, float
         Latitude and longitude values, respectively.
-    timezone: str
+    timezone : str
         Time zone information e.g. 'GMT'.
 
     Returns
     -------
-    A timezone-aware pandas Series of temperature values.
+    dict
+        A dictionary of various weather metrics, including icons.
     """
-    base_url = 'https://api.met.no/weatherapi/locationforecast/2.0/compact'
+    raw_data = requests.get(
+        "https://api.met.no/weatherapi/locationforecast/2.0/compact",
+        params={"lat": lat, "lon": lon},
+        headers={"User-Agent": "wqu_weather_app"},
+    ).json()["properties"]["timeseries"]
 
-    response = requests.get(base_url, params={'lat': lat, 'lon': lon},
-                            headers={'User-Agent': 'wqu_weather_app'})
-    raw_data = response.json()['properties']['timeseries']
+    return dict(
+        current_weather=get_current_weather(
+            raw_data[0]["data"]["instant"]["details"]
+        ),
+        weather_icons=get_weather_icons(raw_data[0]["data"]),
+        temp_time_series=get_temperature_time_series(raw_data, timezone),
+    )
 
-    # Create a Series with temperature data from the nested JSON raw_data
-    time_info = [entry['time'] for entry in raw_data]
-    temp_info = [entry['data']['instant']['details']['air_temperature']
-                 for entry in raw_data]
+
+def get_current_weather(current_weather):
+    """Get the current values for air, cloud and wind metrics.
+
+    Parameters
+    ----------
+    current_weather : dict
+        Weather information for the current hour.
+
+    Returns
+    -------
+    dict
+        Parsed weather information for the current hour.
+    """
+    return {
+        "Air pressure": f"{current_weather['air_pressure_at_sea_level']}hPa",
+        "Air temperature": f"{current_weather['air_temperature']}°C",
+        "Cloud area fraction": f"{current_weather['cloud_area_fraction']}%",
+        "Relative humidity": f"{current_weather['relative_humidity']}%",
+        "Wind direction (from)": f"{current_weather['wind_from_direction']}°",
+        "Wind speed": f"{current_weather['wind_speed']}m/s",
+    }
+
+
+def get_weather_icons(icon_info):
+    """Fetch the appropriate icons to illustrate the weather for the next
+    few hours.
+
+    Parameters
+    ----------
+    icon_info : dict
+        Weather information for the current hour.
+
+    Returns
+    -------
+    dict
+        Weather forecast icons for the next 1, 6 and 12 hours.
+    """
+    return {
+        f"{time}h": icon_info[f"next_{time}_hours"]["summary"]["symbol_code"]
+        for time in [1, 6, 12]
+    }
+
+
+def get_temperature_time_series(weather_info, timezone):
+    """Get air temperature forecasts as a time-zone-aware pandas Series.
+
+    Parameters
+    ----------
+    weather_info : list of dicts
+        Time series data with hourly weather forecasts.
+    timezone : str
+        Time zone information e.g. 'GMT'.
+
+    Returns
+    -------
+    pandas.Series
+        A pandas series of air temperature forecasts.
+    """
+    time_info = [entry["time"] for entry in weather_info]
+    temp_info = [
+        entry["data"]["instant"]["details"]["air_temperature"]
+        for entry in weather_info
+    ]
     temp_data = pd.Series(temp_info, index=time_info)
 
-    # Make the index timezone aware
+    # Make the index time-zone aware
     temp_data.index = pd.to_datetime(temp_data.index).tz_convert(timezone)
+
     return temp_data
 
 
-def plot_forecast(data):
+def plot_forecast(temp_data):
     """Get graphs of air temperature forecasts.
 
     Parameters
     ----------
-    data: pd.Series
-        Temperature values with a timezone-aware date-time index.
+    temp_data: pandas.Series
+        Air temperature time series data.
 
     Returns
     -------
-    A line graph of the 24hr forecast, and a bar graph of the 10-day max & min
-    temperature forecast, stored in text buffers.
+    24h_forecast_graph : io.StringIO
+        A line graph of the 24hr air temperature forecast.
+    10d_forecast_graph : io.StringIO
+        A bar graph of the 10-day max & min temperature forecast.
     """
-    temp24H = data[:24]
-    fig = px.line(y=temp24H, x=temp24H.index.astype(str),
-                  title="24 Hour Forecast")
+    temp24H = temp_data[:24]
+    fig = px.line(
+        y=temp24H, x=temp24H.index.astype(str), title="24 Hour Forecast"
+    )
     # Rename axes labels and disable zoom
-    fig.update_xaxes(title_text='Time', fixedrange=True)
-    fig.update_yaxes(title_text='Air temperature in deg C', fixedrange=True)
-    fig.update_layout(paper_bgcolor='azure', plot_bgcolor='azure')
+    fig.update_xaxes(title_text="Time", fixedrange=True)
+    fig.update_yaxes(title_text="Air temperature in °C", fixedrange=True)
+
+    fig.update_layout(paper_bgcolor="azure", plot_bgcolor="azure")
     fig.update_traces(
-        hovertemplate='<b>Time</b>: %{x}<br><b>Temp</b>: %{y}°C<br>')
+        hovertemplate="<b>Time</b>: %{x}<br><b>Temp</b>: %{y}°C<br>"
+    )
     # Write graph to text buffer
     temp24H_graph = StringIO()
     fig.write_html(
-        temp24H_graph, full_html=False,
-        include_plotlyjs='https://cdn.plot.ly/plotly-basic-1.58.2.min.js')
+        temp24H_graph,
+        full_html=False,
+        include_plotlyjs="https://cdn.plot.ly/plotly-basic-1.58.2.min.js",
+    )
 
-    temp10D = data.resample('1D').agg(['max', 'min'])
-    fig2 = px.bar(temp10D, color_discrete_sequence=['orangered', 'cyan'],
-                  barmode='group', title="10 Day Forecast")
+    temp10D = temp_data.resample("1D").agg(["max", "min"])
+    fig2 = px.bar(
+        temp10D,
+        color_discrete_sequence=["orangered", "cyan"],
+        barmode="group",
+        title="10 Day Forecast",
+    )
     # Rename axes labels and disable zoom
-    fig2.update_xaxes(title_text='Day', fixedrange=True)
-    fig2.update_yaxes(title_text='Air temperature in deg C', fixedrange=True)
-    fig2.update_layout(paper_bgcolor='azure', plot_bgcolor='azure')
+    fig2.update_xaxes(title_text="Day", fixedrange=True)
+    fig2.update_yaxes(title_text="Air temperature in deg C", fixedrange=True)
+
+    fig2.update_layout(paper_bgcolor="azure", plot_bgcolor="azure")
     fig2.update_traces(
-        hovertemplate='<b>Date</b>: %{x}<br><b>Temp</b>: %{y}°C<br>')
+        hovertemplate="<b>Date</b>: %{x}<br><b>Temp</b>: %{y}°C<br>"
+    )
     # Write graph to text buffer
     temp10D_graph = StringIO()
     fig2.write_html(
-        temp10D_graph, full_html=False,
-        include_plotlyjs='https://cdn.plot.ly/plotly-basic-1.58.2.min.js')
+        temp10D_graph,
+        full_html=False,
+        include_plotlyjs="https://cdn.plot.ly/plotly-basic-1.58.2.min.js",
+    )
     return temp24H_graph, temp10D_graph
 
 
 def convert_to_fahr(temp_C):
-    """Convert temperature from degrees Celsius to Fahrenheit."""
+    """Convert temperature from degrees Celsius to Fahrenheit.
+
+    Parameters
+    ----------
+    temp_C : int, float
+        The temperature value in degrees Celcius/centigrade.
+
+    Returns
+    -------
+    int, float
+        The temperature in degrees Fahrenheit.
+    """
     return 9 / 5 * temp_C + 32
 
 
 def get_external_IP_address():
-    """
-    Get the client's IP address via a GET request to an API service.
+    """Get the client's IP address via a GET request to an API service.
 
     Returns
     -------
-    An IPv4 or IPv6 address.
+    str
+        An IPv4 or IPv6 address.
     """
-    return requests.get('https://ident.me/',
-                        headers={'User-Agent': 'wqu_weather_app'}).text
+    return requests.get(
+        "https://ident.me/", headers={"User-Agent": "wqu_weather_app"}
+    ).text
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
 
-    print(greet(sys.argv[1]))  # e.g. python utils.py 8.8.8.8
+    [
+        print(f"{key} --> {value}")
+        for key, value in process_weather_forecast(sys.argv[1]).items()
+    ]  # e.g. python utils.py 8.8.8.8
